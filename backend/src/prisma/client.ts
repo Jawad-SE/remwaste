69c3cb8ac3bbc4288a30a6c96892f1678c150f24
jawad <den.jawaduddinkhan@gmail.com>
2025-07-29 12:28:03 +0300

    fix prisma client error issue

--- backend/src/prisma/client.ts
+++ backend/src/prisma/client.ts
@@ -1,14 +1,13 @@
-// src/config/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
-// Optionally connect at startup for a log:
-prisma.$connect()
+prisma
+  .$connect()
   .then(() => {
-    console.log('Database connected');
+    console.log("Database connected");
   })
-  .catch((err) => {
-    console.error('Prisma connection error:', err);
-    process.exit(1); // Exit if you never want to run without DB
+  .catch((err: unknown) => {
+    console.error("Prisma connection error:", err);
+    process.exit(1);
   });
